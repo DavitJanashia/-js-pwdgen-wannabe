8aebeae124dc9eac1a62e4fff2cb0ead86127f9e
Davit <davit.janashia1@gmail.com>
2020-06-12 16:18:31 +0200

    fine esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// chiedere il NOME
+var nome;
+nome = prompt('Inserisci il tuo nome');
+
+// chiedere il COGNOME
+var cognome;
+cognome = prompt('Inserisci il tuo cognome');
+
+// chiedere il COLORE PREFERITO
+var colorePreferito
+colorePreferito = prompt('Inserisci il tuo colore preferito');
+
+// OUTPUT per l'utente
+var password = nome + cognome + colorePreferito + '19';
+document.getElementById('my-text').innerHTML = password;
